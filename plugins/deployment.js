4cb169b33cbc5640a27a6b686b56a9044d620cec
singjie <singjie@singjie.com>
2011-09-26 16:06:01 +0200

    Fixed handling of message parameter

--- plugins/deployment.js
+++ plugins/deployment.js
@@ -1,8 +1,12 @@
 var f = exports
 f.delegate = function(bot, from, to, message){
-  bot.say(to, 'Deployment on devel is verson blah blah' + message);
-  bot.say(to, 'Deployment on staging is verson blah blah');
-  bot.say(to, 'Deployment on production is verson blah blah');
+  switch(message[0]){
+    case 'deployment':
+     bot.say(to, 'Deployment on devel is verson blah blah' + message);
+     bot.say(to, 'Deployment on staging is verson blah blah');
+     bot.say(to, 'Deployment on production is verson blah blah');
+     break;
+  }
 };
 
 
