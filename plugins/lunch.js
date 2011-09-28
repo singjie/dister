49d6aba06c3ad2f7982a079022e6568b79d202de
singjie <singjie@singjie.com>
2011-09-28 20:11:39 +0200

    Uses db.js as a singleton

--- plugins/lunch.js
+++ plugins/lunch.js
@@ -1,34 +1,29 @@
 // Lunch commands with mongodb
 var db = require ('./../lib/db.js');
-
+var dbOpen = db.Database();
 var f = exports
 f.delegate = function(bot, from, to, message){
   switch(message[0]){
     case "lunch":
       if (message.length > 1){
-        new db.Database(function(err, db){
-          db.collection('lunch', function (err, collection) {
+        dbOpen.collection('lunch', function (err, collection) {
             collection.insert({"location" : message[1]}, function(err, docs) {
               bot.say(to, 'Added ' + message[1] + ' to my brain.');
               console.log('location : ' + message[1]);
-              client.close();
-            });
           });
         });
       }
       else{
         //FIXME: opening multiple connections. crazy
-        new db.Database(function(err, db){
-          db.collection('lunch', function (err, collection) {
-            collection.find(function(err, cursor) {
-              cursor.toArray(function(err, docs) {
-                var rand = (Math.random()*(docs.length-1)).toFixed(0);
-                var doc = docs[rand];
-                if(doc != null) {
-                  console.log(doc.location);
-                  bot.say(to, from +', lunch at ' + doc.location + ' sounds great!');
-                }
-              });
+        dbOpen.collection('lunch', function (err, collection) {
+          collection.find(function(err, cursor) {
+            cursor.toArray(function(err, docs) {
+              var rand = (Math.random()*(docs.length-1)).toFixed(0);
+              var doc = docs[rand];
+              if(doc != null) {
+                console.log(doc.location);
+                bot.say(to, from +', lunch at ' + doc.location + ' sounds great!');
+              }
             });
           });
         });
--- start.js
+++ start.js
@@ -1,10 +1,38 @@
-var repl = require('repl');
 // Starts the bot
 var bot = require('./bot.js');
 // Starts the webserver for receiving webhooks
 var web = require('./web.js');
+var db = require('./lib/db.js');
 
-repl.start();
+var sj = new db.Database;
 //server, nick, channel array
-bot.start('irc.suse.de', 'nicklsjbot', ['#leesingjie']);
-web.start();
+setTimeout(function(){
+  bot.start('irc.suse.de', 'nicklsjbot', ['#leesingjie']);
+  web.start();
+  }, 300);
+
+setTimeout(function(){
+    sj.collection('lunch', function (err, collection) {
+      collection.find(function(err, cursor) {
+        cursor.toArray(function(err, docs) {
+          var doc = docs[0];
+          if(doc != null) {
+            console.log(doc.location);
+          }
+        });
+      });
+    });
+    }, 1000);
+setTimeout(function(){ var abc = new db.Database;
+  abc.collection('lunch', function (err, collection) {
+      collection.find(function(err, cursor) { 
+        cursor.toArray(function(err, docs) { 
+          var doc = docs[1];
+          if(doc != null) { 
+            console.log(doc.location);
+          } 
+        });
+      });
+    });
+}, 3000);
+
