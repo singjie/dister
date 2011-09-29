50a3240129a99b438c4c2f1f757e96990eec65d9
singjie <singjie@singjie.com>
2011-09-29 13:49:12 +0200

    Removes noise polluting console log

--- start.js
+++ start.js
@@ -10,29 +10,3 @@ setTimeout(function(){
   bot.start('irc.suse.de', 'nicklsjbot', ['#leesingjie']);
   web.start();
   }, 300);
-
-setTimeout(function(){
-    sj.collection('lunch', function (err, collection) {
-      collection.find(function(err, cursor) {
-        cursor.toArray(function(err, docs) {
-          var doc = docs[0];
-          if(doc != null) {
-            console.log(doc.location);
-          }
-        });
-      });
-    });
-    }, 1000);
-setTimeout(function(){ var abc = new db.Database;
-  abc.collection('lunch', function (err, collection) {
-      collection.find(function(err, cursor) { 
-        cursor.toArray(function(err, docs) { 
-          var doc = docs[1];
-          if(doc != null) { 
-            console.log(doc.location);
-          } 
-        });
-      });
-    });
-}, 3000);
-
